cddbdc285b8bd78522fbc33780d487a2fb6d0d6d
GioKub <gkublashvili@unisens.ge>
2022-06-20 12:55:30 +0400

    created manage-list and add-list pages with simple tables

--- index.php
+++ index.php
@@ -4,19 +4,44 @@
     <title>Task manager with PHP and MySQL</title>
 </head>
 <body>
+
+    <h1>Task Manager</h1>
     <!-- menu starts here -->
     <div class="menu">
+
         <a href="index.php">Home</a>
+
         <a href="#">To Do </a>
         <a href="#">Doing</a>
         <a href="#">Done</a>
+
+        <a href="manage-list.php">Manage Lists</a>
     </div>
     <!-- menu ends here -->
 
     <!-- Task starts here -->
 
     <div class="all-tasks">
-        <table></table>
+        <a href="#">Add Task</a>
+        <table>
+            <tr>
+                <th>S.N.</th>
+                <th>Task Name</th>
+                <th>Priority</th>
+                <th>Deadline</th>
+                <th>Actions</th>
+            </tr>
+            <tr>
+                <th>1. </th>
+                <th>Design a website</th>
+                <th>Medium</th>
+                <th>20.06.2022</th>
+                <th>
+                    <a href="#">Update</a>
+                    <a href="#">Delete</a>
+                </th>
+            </tr>
+        </table>
     </div>
     <!-- Task ends here -->
 </body>
---
+++ manage-list.php
@@ -0,0 +1,39 @@
+<html>
+
+<head>
+    <title>Task manager with PHP and MySQL</title>
+</head>
+<body>
+    <h1>Task Manager</h1>
+    <h3>Manage Lists Page</h3>
+    <!-- Table to display lists starts here -->
+    <div class="all-lists">
+
+        <a href="add-list.php">Add List</a>
+        <table>
+            <tr>
+                <th>S.N.</th>
+                <th>Task Name</th>
+                <th>Actions</th>
+            </tr>
+            <tr>
+                <th>1. </th>
+                <th>To Do</th>
+                <th>
+                    <a href="#">Update</a>
+                    <a href="#">Delete</a>
+                </th>
+            </tr>
+            <tr>
+                <th>1. </th>
+                <th>Doing</th>
+                <th>
+                    <a href="#">Update</a>
+                    <a href="#">Delete</a>
+                </th>
+            </tr>
+        </table>
+    </div>
+    <!-- Table to display lists starts here -->
+</body>
+<html>
