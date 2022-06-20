6468b1be88e2975141abc04fa15a02910a2d7a2d
GioKub <gkublashvili@unisens.ge>
2022-06-20 11:59:19 +0400

    big bang motherfucker

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>Task manager with PHP and MySQL</title>
+</head>
+<body>
+    <!-- menu starts here -->
+    <div class="menu">
+        <a href="index.php">Home</a>
+        <a href="#">To Do </a>
+        <a href="#">Doing</a>
+        <a href="#">Done</a>
+    </div>
+    <!-- menu ends here -->
+
+    <!-- Task starts here -->
+
+    <div class="all-tasks">
+        <table></table>
+    </div>
+    <!-- Task ends here -->
+</body>
+<html>
